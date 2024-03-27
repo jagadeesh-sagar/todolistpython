c364c9ff6c15fea9f0972e8fb9640fe634b7378d
jagadeesh-sagar <nagarajug176@gmail.com>
2024-03-27 12:28:41 +0530

    had some errors

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-
+import os
 while True:
-    user_action=input('enter one option "add","show","completed"::')
+    user_action=input('enter one option "add","show","exit","completed or use 1,2,3,4::')
     match user_action:
-        case "add":
+        case "add"|"1":
             todos=input("enter a todo list:")+"\n"
             todo=[]
             todo.append(todos)
             file1=open("todo.txt","a")
             file1.writelines(todo)
-
             file1.close()
-        case "show":
+        case "show"|"2":
             file1=open("todo.txt","r")
             c=file1.readlines()
             # c=[i.strip("\n") for i in c ]
@@ -21,6 +20,21 @@ while True:
         case "exit"|"3" :
                print("exited from the todo list")
                break
+        case "completed"|"4":
+            file1=open("todo.txt","r")
+            lines=file1.readlines()
+            file1.close()
+            completed1=input("enter the completed task:")
+
+            for line in lines:
+                if line.strip()!=completed1:
+                     file2=open("todo2.txt","w")
+                     file2.write(line)
+
+            os.replace("todo2.txt","todo.txt")
+            file2.close()
+        case default:
+            print("enter options between 1 to 3")
 
 
 
