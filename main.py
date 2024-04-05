bc7648cec159280e723ee6e8b9f9c58d9217978c
jagadeesh-sagar <nagarajug176@gmail.com>
2024-04-05 15:07:55 +0530

    edit feature is added

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def write_todos(todos):
     with open("todo.txt", "w") as file:
         file.writelines(todos)
 while True:
-    user_action = input('Enter one option\n1.add\n2.show\n3.completed\n4.exit\nCan also use  options names:')
+    user_action = input('Enter one option\n1.add\n2.show\n3.completed\n4.edit\n5.exit\nCan also use  options names:')
     match user_action:
         case "add" | "1":
             todo = input("Enter a todo: ") + "\n"
@@ -21,8 +21,24 @@ while True:
             todos = read_todos()
             completed_task = input("Enter the completed task: ")
             updated_todos = [todo for todo in todos if todo.strip() != completed_task]
+            """"  updated_todos = []
+                    for todo in todos:
+                        if todo.strip() != completed_task:
+                            updated_todos.append(todo)"""
             write_todos(updated_todos)
-        case "exit" | "4":
+
+        case "edit"|"4" :
+              print("")
+              todos=read_todos()
+              for index, todo in enumerate(todos, start=1):
+                print(f"{index}. {todo.strip()}")
+              num=int(input("enter a index  :"))-1
+              updated_todos = [todo for todo in todos]
+              updated_todos[num]=input("enter the todo: ")+'\n'
+              write_todos(updated_todos)
+              for index, todo in enumerate(updated_todos, start=1):
+                print(f"{index}. {todo.strip()}")
+        case "exit" | "5":
             print("Exited from the todo list")
             break
         case _:
