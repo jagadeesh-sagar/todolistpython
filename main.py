96da00444e32c045e0994ea6667fdaa291f0809d
jagadeesh-sagar <nagarajug176@gmail.com>
2024-04-07 23:09:53 +0530

    handled some of bugs

--- main.py
+++ main.py
@@ -8,20 +8,19 @@ def write_todos(todos):
 while True:
    user_action = input('Type add,show,completed,edit,exit::')
    user_action.strip()
-
-   if "add" in user_action:
+   if user_action.startswith("add"):
         todo = user_action[4:]+"\n"
         todos = read_todos()
         todos.append(todo)
         write_todos(todos)
-   elif "show" in user_action:
+   elif user_action.startswith("show"):
         todos = read_todos()
         for index, todo in enumerate(todos, start=1):
             print(f"{index}. {todo.strip()}")
-   elif "completed" in user_action:
+   elif user_action.startswith("completed"):
         todos = read_todos()
         completed_task = user_action[10:]
-        updated_todos = [todo for todo in todos if todo.strip() != completed_task] # list comprehension
+        updated_todos = [todo for todo in todos if todo.strip() != completed_task ] # list comprehension
         """  updated_todos = []
                 for todo in todos:
                     if todo.strip() != completed_task:
@@ -31,20 +30,27 @@ while True:
             write_todos(todos)""" # another way of doing
         write_todos(updated_todos)
 
-   elif "edit" in user_action:
-          print("")
-          todos=read_todos()
-          for index, todo in enumerate(todos, start=1):
-            print(f"{index}. {todo.strip()}")
-          num=int(input("enter a index  :"))-1
-          updated_todos = [todo for todo in todos]
-          updated_todos[num]=input("enter the todo: ")+'\n'
-          write_todos(updated_todos)
-          for index, todo in enumerate(updated_todos, start=1):
-            print(f"{index}. {todo.strip()}")
-   elif "exit" in user_action :
+   elif user_action.startswith("edit"):
+       try:
+              print("")
+              todos=read_todos()
+              for index, todo in enumerate(todos, start=1):
+                print(f"{index}. {todo.strip()}")
+              num=int(user_action[5:])-1
+              updated_todos = [todo for todo in todos]
+              updated_todos[num]=input("enter the todo: ")+'\n'
+              write_todos(updated_todos)
+              for index, todo in enumerate(updated_todos, start=1):
+                print(f"{index}. {todo.strip()}")
+       except ValueError:
+            print("Your command is not valid")
+            # continue
+       except IndexError:
+           print("index is not valid ")
+
+   elif user_action.startswith("exit"):
         print("Exited from the todo list")
         break
    else:
-        print("Enter options between 1 to 4")
+        print("Enter valid options:")
 
